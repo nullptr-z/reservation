cc24f2cd6af519bd56625df7fba2f6d6ca26bdd5
zheng <zhouzheng0646@qq.com>
2023-03-10 18:09:49 +0800

    fix: add reserve test

--- abi/src/lib.rs
+++ abi/src/lib.rs
@@ -1,3 +1,16 @@
 mod pb;
 
 pub use pb::*;
+
+use chrono::DateTime;
+use prost_types::Timestamp;
+
+pub fn convert_str_to_Timestamp(dt: &str) -> Timestamp {
+    let dt = DateTime::parse_from_str(dt, "%Y-%m-%d %H:%M:%S").unwrap();
+    let ts = dt.timestamp_millis();
+
+    Timestamp {
+        seconds: (ts / 1000) as i64,
+        nanos: ((ts % 1000) * 1_000_000) as i32,
+    }
+}
--- reservation/src/manager.rs
+++ reservation/src/manager.rs
@@ -2,6 +2,7 @@ use crate::{error, ReservationId, ReservationManager, Rsvp};
 use chrono::NaiveDateTime;
 use sqlx::{postgres::types::PgRange, Row};
 
+#[warn(non_snake_case)]
 #[async_trait::async_trait]
 impl Rsvp for ReservationManager {
     async fn reserve(
@@ -22,7 +23,7 @@ impl Rsvp for ReservationManager {
 
         let timespan: PgRange<_> = (start..end).into();
         let id = sqlx::query(
-            "INSERT INTO reservation(user_id, resource_id, timespan, note ,status) VALUES($1,$2,$3,$4,$5) RETURNING id"
+            "INSERT INTO reservation(user_id, resource_id, timespan, note ,status) VALUES($1, $2, $3, $4,   $5) RETURNING id"
         )
         .bind(rsvp.user_id.clone())
         .bind(rsvp.resource_id.clone())
@@ -65,5 +66,35 @@ impl Rsvp for ReservationManager {
         query: abi::ReservationQuery,
     ) -> Result<Vec<abi::Reservation>, error::ReservationError> {
         todo!()
+    }
+}
+
+impl ReservationManager {
+    fn new(pool: sqlx::PgPool) -> Self {
+        Self { pool }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{ReservationManager, Rsvp};
+    use abi::convert_str_to_Timestamp;
+
+    #[sqlx_database_tester::test(pool(variable = "migrated_pool", migrations = "../migrations"))]
+    async fn reserve_should_work_for_volid_window() {
+        let manager = ReservationManager::new(migrated_pool.clone());
+        let rsvp = abi::Reservation {
+            id: "".to_string(),
+            user_id: "zz id".to_string(),
+            resource_id: "ocean-view-room-713".to_string(),
+            start: Some(convert_str_to_Timestamp("2023-03-10 17:20:35")),
+            end: Some(convert_str_to_Timestamp("2023-03-11 17:20:35")),
+            // end: NaiveDateTime::parse_from_str("2023-03-11 16:53:56", &fmt).unwrap(),
+            note: "我明天晚上7点入住".to_string(),
+            status: abi::ReservationStatus::Pending as i32,
+        };
+
+        let rsvp = manager.reserve(rsvp).await.unwrap();
+        assert_ne!(rsvp.id, "");
     }
 }
