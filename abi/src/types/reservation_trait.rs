bf7a80bf938db0cf04466b75d9cc7771b928797f
zheng <zhouzheng0646@qq.com>
2023-03-15 10:14:46 +0800

    done reserve_change_status_should_work test

--- abi/src/types/reservation_trait.rs
+++ abi/src/types/reservation_trait.rs
@@ -1,6 +1,6 @@
-use std::fmt::Display;
+use std::{fmt::Display, ops::Bound};
 
-use chrono::NaiveDateTime;
+use chrono::{DateTime, NaiveDateTime, Utc};
 use sqlx::{
     postgres::{types::PgRange, PgRow},
     types::Uuid,
@@ -43,7 +43,8 @@ impl From<RsvpStatus> for ReservationStatus {
 
 impl FromRow<'_, PgRow> for Reservation {
     fn from_row(row: &PgRow) -> Result<Self, sqlx::Error> {
-        let time_range: PgRange<NaiveDateTime> = row.get("teimespan");
+        let time_range: PgRange<DateTime<Utc>> = row.get("timespan");
+        println!("time_range->{:?}", time_range);
         let n_d_r: NaiveDateRange = time_range.into();
 
         let status: RsvpStatus = row.get("status");
@@ -62,11 +63,26 @@ impl FromRow<'_, PgRow> for Reservation {
     }
 }
 
-// 有过痛苦、针扎，我还是无法装作不知道，现在我决定离开了；如果他能就是你命中注定的那个人，那么我也祝你幸福
 struct NaiveDateRange {
     start: Option<NaiveDateTime>,
     end: Option<NaiveDateTime>,
 }
+
+impl From<PgRange<DateTime<Utc>>> for NaiveDateRange {
+    fn from(pg_r: PgRange<DateTime<Utc>>) -> Self {
+        let ret_time = |date: Bound<DateTime<Utc>>| match date {
+            std::ops::Bound::Included(v) => Some(v.naive_utc()),
+            std::ops::Bound::Excluded(v) => Some(v.naive_utc()),
+            std::ops::Bound::Unbounded => None,
+        };
+
+        let start = ret_time(pg_r.start);
+        let end = ret_time(pg_r.end);
+        Self { start, end }
+    }
+}
+
+// 有过痛苦、针扎，我还是无法装作不知道，现在我决定离开了；如果他能就是你命中注定的那个人，那么我也祝你幸福
 
 impl From<PgRange<NaiveDateTime>> for NaiveDateRange {
     fn from(pg_r: PgRange<NaiveDateTime>) -> Self {
